f670284cf11642f239c80ed13c12652b05811b45
rohitprocode <rohit990797@gmail.com>
2023-10-25 18:21:59 +0530

    Style and design

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,20 +4,25 @@ import './MainContainer.css'
 function MainContainer() {
   return (
     <div className='Container'>
-      <div className="text-content">
+        <div className='content-field' >
+      <div className="text-content-field">
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
       <div className="text-content-btn">
-        <button>Shop Now</button>
-        <button>Category</button>
+        <button className='btn1'>Shop Now</button>
+        <button className='btn2'>Category</button>
       </div>
       <div className="selling-app">
+      <p>Also Available On</p>
+      <div className="site-img">
         <img src="./images/flipkart.png" alt="flipkart-logo" />
         <img src="./images/amazon.png" alt="amazon-logo" />
+        </div>
       </div>
       </div>
-      <div className="img-content">
+      <div className="img-content-field">
         <img src="./images/shoe_image.png" alt="" />
+      </div>
       </div>
     </div>
   )
